ad5349fc08a1c33f3d97703e0f9b4ffc388f59d2
Harshit Sharma <Harshit.Sharma@lft.com>
2025-08-24 23:45:51 +0530

    Updatd

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ const server = http.createServer(app);
 
 
 app.use((req, res, next) => {
-  res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+  // res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+  // res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
   next();
 });
 
